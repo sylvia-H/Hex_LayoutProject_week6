e77ef9c50a46157135ae2f098599b31d5ac28443
sylvia-H <sylvie11gaia@gmail.com>
2021-08-29 23:32:56 +0800

    調整 swiper.js autoplay 設定

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,15 +1,19 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-    spaceBetween: 30,
-    breakpoints: {
-      768:{
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
+  spaceBetween: 30,
+  breakpoints: {
+    768:{
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-    autoplay: {
-      delay: 1800,
-      disableOnInteraction: true,
-    }
+  },
+  loop:true,
+  initialSlide : 1,
+  grabCursor: true,
+  autoplay: {
+    delay: 1600,
+    disableOnInteraction: false,
+    reverseDirection: true,
+  }
   
 });

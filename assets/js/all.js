10cc40f6fbb33b6fdd5ec18f60b87d12dd9797d1
sylvia-H <sylvie11gaia@gmail.com>
2021-08-29 14:17:10 +0800

    Update 2021-08-29T06:16:23.436Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,17 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+  autoplay: {
+    delay: 1800,
+    disableOnInteraction: false
+  }
 });
 //# sourceMappingURL=all.js.map

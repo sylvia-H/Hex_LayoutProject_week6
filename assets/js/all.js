ff782d20a236659976f0a10dd89ee32e9ab96079
sylvia-H <sylvie11gaia@gmail.com>
2021-08-30 20:43:01 +0800

    Update 2021-08-30T12:42:21.289Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,21 +1,19 @@
 "use strict";
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  initialSlide: 1,
+  grabCursor: true,
   breakpoints: {
     768: {
       slidesPerView: 3,
       spaceBetween: 30
     }
   },
-  loop: true,
-  initialSlide: 1,
-  grabCursor: true,
   autoplay: {
-    delay: 1600,
-    disableOnInteraction: false,
-    reverseDirection: true
+    delay: 1600
   }
 });
 //# sourceMappingURL=all.js.map

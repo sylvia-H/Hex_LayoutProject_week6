0ddfd5f7f4f191bb2985d937f7696283173030a5
sylvia-H <sylvie11gaia@gmail.com>
2021-08-30 22:22:19 +0800

    調整 swiper & 增加禁止選取

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,14 +1,17 @@
-var swiper = new Swiper(".swiper-container", {
-  loop:true,
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
   slidesPerView: 'auto',
   spaceBetween: 15,
   grabCursor: true,
-  initialSlide : 1,
   breakpoints: {
     768:{
       slidesPerView: 3,
       spaceBetween: 30,
     },
+  },
+  pagination:{
+    el: ".swiper-pagination",
+    clickable: true,
   },
   autoplay: {
     delay: 1600,

ec2b5304dd1f07d0fed5f879672e9a9d1c092ca9
sylvia-H <sylvie11gaia@gmail.com>
2021-08-30 20:41:04 +0800

    調整 swiper & 購物車相關頁面建置中

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,19 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
+  loop:true,
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  initialSlide : 1,
+  grabCursor: true,
   breakpoints: {
     768:{
       slidesPerView: 3,
       spaceBetween: 30,
     },
   },
-  loop:true,
-  initialSlide : 1,
-  grabCursor: true,
   autoplay: {
     delay: 1600,
-    disableOnInteraction: false,
-    reverseDirection: true,
   }
   
 });

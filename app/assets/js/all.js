fa18500ca35040f226ecfa362eafd1b848d6c41b
sylvia-H <sylvie11gaia@gmail.com>
2021-08-30 21:14:31 +0800

    調整 swiper & 購物車相關頁面建置中

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -10,6 +10,10 @@ var swiper = new Swiper(".swiper-container", {
       spaceBetween: 30,
     },
   },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
   autoplay: {
     delay: 1600,
   }
